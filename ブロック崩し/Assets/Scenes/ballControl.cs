d9722a134abff22b1580ae796b97acaeafae781b
test <fca>
2024-06-04 12:28:07 +0900

    ボール動き

---
+++ ブロック崩し/Assets/Scenes/ballControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ballControl : MonoBehaviour
+{
+    public float speed = 5;    // Start is called before the first frame update
+    Rigidbody myrigbodoy;
+    void Start()
+    {
+        myrigbodoy = GetComponent<Rigidbody>();
+        myrigbodoy.velocity =new Vector3(speed,speed,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
